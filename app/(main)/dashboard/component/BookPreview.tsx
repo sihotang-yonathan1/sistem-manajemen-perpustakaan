02c735fc23fe3522fec519905a8bed1a74242125
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-01 19:30:05 +0800

    [dashboard] move BookPreview in seperate folder

---
+++ app/(main)/dashboard/component/BookPreview.tsx
@@ -0,0 +1,15 @@
+export default function BookPreview({title, description}: {title: string, description?: string | null}){
+    return (
+        <div className="bg-sky-300 m-2 p-2">
+            <div>
+                <h3 className="font-semibold">{title}</h3>
+            </div>
+            <div>
+                <p className="text-sm">{description}</p>
+            </div>
+            <div className="flex justify-center my-1">
+                <button className="bg-orange-500 text-white p-1">Read</button>
+            </div>
+        </div>
+    )
+}
--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,18 +1,4 @@
-export function BookPreview({title, description}: {title: string, description?: string | null}){
-    return (
-        <div className="bg-sky-300 m-2 p-2">
-            <div>
-                <h3 className="font-semibold">{title}</h3>
-            </div>
-            <div>
-                <p className="text-sm">{description}</p>
-            </div>
-            <div className="flex justify-center my-1">
-                <button className="bg-orange-500 text-white p-1">Read</button>
-            </div>
-        </div>
-    )
-}
+import BookPreview from "./component/BookPreview";
 
 export default function DashboardPage(){
     return (
