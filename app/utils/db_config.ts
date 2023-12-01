909526b313aab5a8a34309f45bd171a02a65fa9e
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-01 13:04:46 +0800

    [db_config] reconfigure prisma

--- app/utils/db_config.ts
+++ app/utils/db_config.ts
@@ -8,7 +8,8 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
+export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
