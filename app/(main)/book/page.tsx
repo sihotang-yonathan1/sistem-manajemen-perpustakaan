2676a3255b09013733db83be8d60e7b89ffd5dee
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-01 21:54:42 +0800

    [app/book] remove complicated and cyclic if block

--- app/(main)/book/page.tsx
+++ app/(main)/book/page.tsx
@@ -6,30 +6,12 @@ import { cookies } from "next/headers"
 export default async function UserBookPage(){
     const sessionId = cookies().get('X-SESSION-ID')?.value
     const sessionData = await getSession(Number(sessionId))
-    const username = sessionData?.username
     
-    let userBook = []
-    if (username !== null && username !== undefined){
-        const user_info = await getAccountInfoByUsername(username)
-        if (user_info.length > 0){
-            const userBooks = await getUserBooks(user_info[0].id)
-            if (userBooks !== null){
-                userBook = userBooks
-            }
-        }
-    }
     return (
         <>
             <div className="flex bg-slate-400 p-2 justify-center">
                 <p className="font-bold">My Book</p>
             </div>
-            {
-                userBook.map((value, index) => (
-                    <div key={index}>
-                        <p>{value}</p>
-                    </div>
-                ))
-            }
         </>
     )
 }
