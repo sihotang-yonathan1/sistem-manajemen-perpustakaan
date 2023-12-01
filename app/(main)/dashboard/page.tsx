4f0eac91259e804017aa78510ed0a42b68ed99d4
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-01 18:29:31 +0800

    [app/dashboard] create basic ui in dashboard

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,31 @@
+export function BookPreview({title, description}: {title: string, description?: string | null}){
+    return (
+        <div className="bg-sky-300 m-2 p-2">
+            <div>
+                <h3 className="font-semibold">{title}</h3>
+            </div>
+            <div>
+                <p className="text-sm">{description}</p>
+            </div>
+            <div className="flex justify-center my-1">
+                <button className="bg-orange-500 text-white p-1">Read</button>
+            </div>
+        </div>
+    )
+}
+
 export default function DashboardPage(){
     return (
         <div>
-            <p>Hello World</p>
+            <div className="flex bg-slate-400 justify-center p-2">
+                <p className="font-bold">Dashboard</p>
+            </div>
+            <div className="flex overflow-x-auto">
+                <BookPreview title="Title" description="description of this book"/>
+                <BookPreview title="Title" description="description of this book"/>
+                <BookPreview title="Title" description="description of this book"/>
+                <BookPreview title="Title" description="description of this book"/>
+            </div>
         </div>
     )
 }
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,6 @@
 export default function MainLayout({children}: {children: React.ReactNode}){
     return (
         <div>
-            <p>Layout</p>
             {children}
         </div>
     )
