c14a78aebf0650edfb5120900c0d8dec52d4b39c
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-01 14:02:05 +0800

    [api/account] fix typo and fix it to deleteAccountByUsername

--- app/api/v1/account/route.ts
+++ app/api/v1/account/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { AddAccount, deletAccountByUsername } from "./account";
+import { AddAccount, deleteAccountByUsername } from "./account";
 
 export async function POST(request: NextRequest){
     let request_json = await request.json()
@@ -10,6 +10,6 @@ export async function POST(request: NextRequest){
 export async function DELETE(request: NextRequest){
     let request_json = await request.json()
     // TODO: implement authorization using API-KEY
-    let response = await deletAccountByUsername(request_json['username'])
+    let response = await deleteAccountByUsername(request_json['username'])
     return new NextResponse(JSON.stringify(response))
 }
