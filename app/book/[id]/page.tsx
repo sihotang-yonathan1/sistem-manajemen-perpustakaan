accc58b3dd9f963c5dbf56cbeb3f74e7b3cf500a
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-02 13:19:55 +0800

    [book/[id]] Add basic UI to the page

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function IndividualBookPage({params}: {params: {id: number}}){
+import { getBookInfo } from "@/app/api/v1/book/book"
+
+export default async function IndividualBookPage({params}: {params: {id: number}}){
+    const bookInfo = await getBookInfo(Number(params.id))
     return (
-        <div>
-            <p>Hello from Book page: {params.id}</p>
+        <div className="m-2 py-2">
+            <div className="flex">
+                <div className="aspect-square bg-slate-200 p-5 mx-3 flex justify-center items-center">
+                    <p>Image Here</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="font-semibold text-lg">{bookInfo?.title}</p>
+                    <p>{bookInfo?.description}</p>
+                </div>
+            </div>
+            <div className="my-2 mx-3">
+                <p>Book Volume</p>
+                <div>
+                    <p>Volume 1</p>
+                    <p>Volume 2</p>
+                </div>
+            </div>
         </div>
     )
 }
