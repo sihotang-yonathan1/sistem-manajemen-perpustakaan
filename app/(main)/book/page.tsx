6a908b2736ecfa5ab12157129cd5fe63a1d1ed79
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-02 11:52:49 +0800

    [book] Add basic UI to implement book section and its item

--- app/(main)/book/page.tsx
+++ app/(main)/book/page.tsx
@@ -2,6 +2,23 @@ import { getAccountInfoByUsername } from "@/app/api/v1/account/account"
 import { getSession } from "@/app/api/v1/session/session"
 import { getUserBooks } from "@/app/api/v1/user_book/user_book"
 import { cookies } from "next/headers"
+import BookPreview from "../dashboard/component/BookPreview";
+import React from "react"
+
+
+export function BookSection({title, children}: {title: string, children: React.ReactNode}){
+    return (
+        <div className="bg-slate-200">
+            <div className="p-2">
+                <p className="font-semibold">{title}</p>
+            </div>
+            <div className="overflow-x-auto flex">
+                {children}
+            </div>
+        </div>
+    )
+}
+
 
 export default async function UserBookPage(){
     const sessionId = cookies().get('X-SESSION-ID')?.value
@@ -11,6 +28,12 @@ export default async function UserBookPage(){
         <>
             <div className="flex bg-slate-400 p-2 justify-center">
                 <p className="font-bold">My Book</p>
+            </div>
+            <div className="m-2 p-1">
+                <BookSection title="Buku dipinjam">
+                    <BookPreview title="Buku 1" description="Hello world"/>
+                    <BookPreview title="Buku 2" description="Hello world 2"/>
+                </BookSection>
             </div>
         </>
     )
