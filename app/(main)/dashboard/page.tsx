ff4f1dede5f3d440dc5cb5bfea0f0d357e894ae3
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-02 12:38:11 +0800

    [dashboard] Add book data based on the database

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,19 +1,23 @@
 import { cookies } from "next/headers";
 import BookPreview from "./component/BookPreview";
 import { redirect } from "next/navigation";
+import { getAllBook } from "@/app/api/v1/book/book";
 
-export default function DashboardPage(){
+export default async function DashboardPage(){
     const sessionId = cookies().get('X-SESSION-ID')?.value
+    const allBooks = await getAllBook() ?? []
+    
     return (
         <div>
             <div className="flex bg-slate-400 justify-center p-2">
                 <p className="font-bold">Dashboard</p>
             </div>
             <div className="flex overflow-x-auto">
-                <BookPreview title="Title" description="description of this book"/>
-                <BookPreview title="Title" description="description of this book"/>
-                <BookPreview title="Title" description="description of this book"/>
-                <BookPreview title="Title" description="description of this book"/>
+                {allBooks.map((value, index)=> (
+                    <div key={index}>
+                        <BookPreview title={value.title ?? ""} description={value.description}/>
+                    </div>
+                ))}
             </div>
         </div>
     )
