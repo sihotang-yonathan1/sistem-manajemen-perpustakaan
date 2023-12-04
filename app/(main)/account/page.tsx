2f9b42487bde9a836fa3690bddc0d6f29c376d47
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-04 10:08:49 +0800

    [account] Add UI to show available account

--- app/(main)/account/page.tsx
+++ app/(main)/account/page.tsx
@@ -1,7 +1,29 @@
-export default function AccountManagementPage(){
+import { getAllAccountInfo } from "@/app/api/v1/account/account"
+
+export default async function AccountManagementPage(){
+    const users = await getAllAccountInfo()
     return (
-        <div className="flex bg-slate-400 p-2 justify-center">
-            <p className="font-bold">Account Management</p>
-        </div>
+        <>
+            <div className="flex bg-slate-400 p-2 justify-center">
+                <p className="font-bold">Account Management</p>
+            </div>
+            <div>
+                {users.map((value, index) => (
+                    <div key={value.id} className="p-2">
+                        {   value.role === 'admin'
+                            ? <div className="bg-green-200 px-2 py-3">
+                                <p className="font-semibold">{value.username}</p>
+                                <p className="text-xs">{value.role}</p>
+                              </div>
+                            : <div className="px-2 py-3 bg-blue-200">
+                                <p className="font-semibold">{value.username}</p>
+                                <p className="text-xs">{value.role}</p>
+                              </div>
+
+                        }
+                    </div>    
+                ))}
+            </div>
+        </>
     )
 }
