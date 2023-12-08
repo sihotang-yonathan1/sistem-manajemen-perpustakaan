4a2c42b63e0803af5f3c90d76b400f8611fe5cb4
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-08 10:43:23 +0800

    [app/Sidebar] Set sidebar based on user role

--- app/(main)/Sidebar.tsx
+++ app/(main)/Sidebar.tsx
@@ -1,34 +1,51 @@
 import Link from "next/link"
 import { getSession } from "../api/v1/session/session"
 import { cookies } from "next/headers"
+import { getAccountInfoByUsername } from "../api/v1/account/account"
 
 
 const sidebar_menu = [
     {
         name: 'dashboard',
-        url_path: '/dashboard'
+        url_path: '/dashboard',
+        isAdminPrivilegeRequired: false
     }, {
         name: 'My book',
-        url_path: '/book'
+        url_path: '/book',
+        isAdminPrivilegeRequired: false
     }, {
         name: 'Account',
-        url_path: '/account'
+        url_path: '/account',
+        isAdminPrivilegeRequired: true
     }
 ]
 
 export default async function Sidebar({sessionId}: {sessionId: number}){
     const sessionData = await getSession(sessionId)
+    const username = sessionData?.username
+    let userRole = 'user'
+    if (username !== null || username !== undefined){
+        const userInfo = await getAccountInfoByUsername(username as string)
+        userRole = userInfo[0].role ?? 'user'
+    }
     return (
         <div className=" flex flex-col p-3 bg-slate-500 h-screen relative">
             <div className="flex justify-center mb-2 flex-col items-center">
                 <p className="text-white">Welcome</p>
                 <p className="text-white uppercase">{sessionData?.username}</p>
             </div>
-            {sidebar_menu.map((value) => (
-                <Link href={value.url_path} key={value.url_path} className="bg-white mb-2 p-1">
-                    <button className="capitalize">{value.name}</button>
-                </Link>
-            ))}
+            {sidebar_menu.map((value) => {
+                if (value.isAdminPrivilegeRequired && userRole !== 'admin'){
+                    
+                }
+                else {
+                    return (
+                        <Link href={value.url_path} key={value.url_path} className="bg-white mb-2 p-1">
+                            <button className="capitalize">{value.name}</button>
+                        </Link>
+                    )
+                }
+            })}
             <Link href="/logout" className="flex bg-orange-400 mt-auto justify-center"> 
                 <button className="px-2 py-2" >Logout</button>
             </Link>
