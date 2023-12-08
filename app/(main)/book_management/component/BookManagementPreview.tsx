9ba4869a6a98ca8c9f41bd512a6e419f827dd55c
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-08 15:14:43 +0800

    [book_management] reflect delete UI with the database

--- app/(main)/book_management/component/BookManagementPreview.tsx
+++ app/(main)/book_management/component/BookManagementPreview.tsx
@@ -1,20 +1,30 @@
+"use client"
+
 import Link from "next/link";
+import BookPreview from "../../dashboard/component/BookPreview";
+import { useRouter } from "next/navigation";
 
 export default function BookManagementPreview({title, description, bookId}: {title: string, description?: string | null, bookId: number}){
+    const router = useRouter()
+    function handleDeleteBook(){
+        const deleteBookFunction = async () => {
+            await fetch(`http://localhost:3000/api/v1/book`, {
+                method: "DELETE",
+                credentials: "include",
+                body: JSON.stringify({
+                    'book_id': bookId
+                })
+            })
+        }
+        deleteBookFunction()
+        router.refresh()
+    }
+    
     return (
         <div className="bg-sky-300 m-2 p-2 h-full flex flex-col">
-            <Link href={`/book/${bookId}`} className="flex flex-col w-full flex-grow">
-                <div className="bg-sky-300 m-2 p-2 h-full flex flex-col">
-                    <div className="flex justify-center">
-                        <h3 className="font-semibold">{title}</h3>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <p className="text-sm">{description}</p>
-                    </div>
-                </div>
-            </Link>
+            <BookPreview title={title} description={description} bookId={bookId}/>
             <div className="flex flex-col">
-                <button className="bg-red-500 m-1 text-white py-1">Delete</button>
+                <button className="bg-red-500 m-1 text-white py-1" onClick={handleDeleteBook}>Delete</button>
             </div>
         </div>
     )
