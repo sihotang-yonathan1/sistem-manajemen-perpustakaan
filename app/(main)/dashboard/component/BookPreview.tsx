2cb9d8e855d6783e2a5298f7cc89ecf9b310666b
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-09 13:16:49 +0800

    [dashboard] Addgit diff

--- app/(main)/dashboard/component/BookPreview.tsx
+++ app/(main)/dashboard/component/BookPreview.tsx
@@ -1,3 +1,5 @@
+// import TestImage from "../../../../assets/WhatsApp Image 2023-12-09 at 12.39.20.jpeg"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function BookPreview({title, description, bookId}: {title: string, description?: string | null, bookId: number}){
@@ -5,10 +7,16 @@ export default function BookPreview({title, description, bookId}: {title: string
         <Link href={`/book/${bookId}`} className="flex flex-col w-full flex-grow">
             <div className="bg-sky-300 m-2 p-2 h-full flex flex-col">
                 <div className="flex justify-center">
+                    {/* <Image src={TestImage} height={100} width={100} alt="book image"/> */}
+                </div>
+                <div className="flex justify-center">
                     <h3 className="font-semibold">{title}</h3>
                 </div>
                 <div className="flex justify-center items-center">
                     <p className="text-sm">{description}</p>
+                </div>
+                <div className="flex justify-center">
+                    <p>Author: A</p>
                 </div>
             </div>
         </Link>
