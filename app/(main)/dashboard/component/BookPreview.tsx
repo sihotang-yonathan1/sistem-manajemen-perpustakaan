ab6c12d6010f5bc21378e9a1eff0a2f3e817211b
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-09 16:01:46 +0800

    [BookPreview] set grey box as the susbtitute of the absence of the imageUrl of the book

--- app/(main)/dashboard/component/BookPreview.tsx
+++ app/(main)/dashboard/component/BookPreview.tsx
@@ -9,13 +9,19 @@ export default function BookPreview({
     return (
         <Link href={`/book/${bookId}`} className="flex flex-col w-full flex-grow">
             <div className="bg-sky-300 m-2 p-2 h-full flex flex-col">
-                {imageUrl !== null && <div className="flex justify-center">
+                {imageUrl !== null 
+                 ? <div className="flex justify-center">
                     <Image 
                         src={imageUrl} 
                         height={300} 
                         width={100} 
                         alt="book image"
                     />
+                </div>
+                : <div className="flex flex-col items-center">
+                    <div className="flex justify-center bg-slate-400 h-[150px] w-[150px] items-center">
+                        <p>No Image</p>
+                    </div>
                 </div>
                 }
                 <div className="flex justify-center my-2">
